5addab121c8f13589573cdab5f2e8a1e70a6371a
JuanAnRz <juancitoarodriguez1@gmail.com>
2025-04-28 09:30:14 -0300

    img-panel

--- js/panel.js
+++ js/panel.js
@@ -11,10 +11,30 @@ pel.addEventListener('click',()=>{
 
     const sub=document.getElementById('sub').addEventListener('click', ()=>{
         main.innerHTML=`
-            
+            <div id='cont'>
+                <div id='contImg'>
+                    <img id='img' src='' alt=''>
+                </div>
+                
+                <label for='file' id="fils">Subir imagen</label>
+                <input name=file class="ins" id='file' type="file" alt="">
+            </div>
         `
+        const img=document.getElementById('img')
+        file.addEventListener('change', e=>{
+            if(e.target.files[0]){
+                const reader= new FileReader()
+                reader.onload =function(e){
+                    img.src= e.target.result
+                }
+                reader.readAsDataURL(e.target.files[0])
+            }else{
+                img.src=''
+            }
+        })
     })
     const baj=document.getElementById('baj')
 
 
 })
+
